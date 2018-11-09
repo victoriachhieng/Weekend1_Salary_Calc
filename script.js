1050edd28a184f5f02968855fcf64e66d7aa5ba6
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-11-09 15:08:48 -0600

    add input elements

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ $(document).ready( readyNow );
     function readyNow (){
         console.log('in jQuery');
 } // end document ready
+
+ // class for employees
+class Employee{
+    constructor ( firstName, lastName, idNumber, jobTitle){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = idNumber;
+    this.jobTitle = jobTitle;
+    this.annualSalary = annualSalary;
+    } // end constructor 
+} // end Employee class
